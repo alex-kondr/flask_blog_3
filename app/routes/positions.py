8f9968ed0a8465fbec6c619d120375ed0a939a77
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-10-10 20:39:40 +0300

    first commit

---
+++ app/routes/positions.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template, request, redirect
+
+from app.db import Session, Position
+from app.data.password import ADMIN_PASS
+
+
+position_route = Blueprint("positions", __name__, url_prefix="/positions/")
+
+
+@position_route.get("/")
+@position_route.post("/")
+def add_position():
+    msg = ""
+    block = False
+    if request.method == "POST":
+        name = request.form.get("name")
+
+        password = request.form.get("password")
+        if password == ADMIN_PASS:
+            with Session() as session:
+                position = Position(name=name)
+                session.add(position)
+                session.commit()
+                msg = "Посада успішно додана"
+        else:
+            block = True
+
+    return render_template("position.html", block=block, msg=msg)
---
+++ app/routes/user.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request
+
+from app.db import Base, Session, User, Position
+from app.data.password import ADMIN_PASS
+
+
+user_route = Blueprint("users", __name__, url_prefix="/users/")
+
+
+@user_route.get("/")
+@user_route.post("/")
+def add_user():
+    block = False
+    msg = ""
+    with Session() as session:
+        if request.method == "POST":
+            first_name = request.form.get("first_name")
+            last_name = request.form.get("last_name")
+            position_id = request.form.get("position_id")
+
+            if request.form.get("password") == ADMIN_PASS:
+                user = User(first_name=first_name, last_name=last_name, position_id=position_id)
+                session.add(user)
+                session.commit()
+                msg = "Користувача успішно додано"
+            else:
+                block = True
+
+        positions = session.query(Position).all()
+        context = {
+            "positions": positions,
+            "block": block,
+            "msg": msg
+        }
+        return render_template("user.html", **context)
